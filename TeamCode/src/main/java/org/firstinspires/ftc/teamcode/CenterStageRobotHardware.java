380db69efd089e14f93038e3054f71750e84830a
J18035 <jmffive2007@gmail.com>
2023-10-06 19:46:55 -0400

    Commented both CenterStageRobotHardware and CenterStageRobotHardwareTest.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageRobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageRobotHardware.java
@@ -10,6 +10,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class CenterStageRobotHardware {
 
+    // DECLARE ALL VARIABLES HERE
+
+
+    //We create space holders here. So we can declare in the init function
     public DcMotor FrontLeft;
     public DcMotor FrontRight;
     public DcMotor BackLeft;
@@ -24,6 +28,7 @@ public class CenterStageRobotHardware {
 
 
     public void Init(HardwareMap map){
+        //We assign the actual motors to the variables we created above
         FrontLeft = map.get(DcMotor.class, "Fleft");
         FrontRight = map.get(DcMotor.class, "Fright");
         BackRight = map.get(DcMotor.class, "Bright");
@@ -31,7 +36,8 @@ public class CenterStageRobotHardware {
         Claw = map.get(Servo.class, "Claw");
         Imu = map.get(BNO055IMU.class, "imu");
 
-        BNO055IMU.Parameters newImuParameters;
+        //Here we set the Orientation of the imu which is built into the control hub otherwise know as the "Chub"
+       /* BNO055IMU.Parameters newImuParameters;
 
         newImuParameters = new BNO055IMU.Parameters(
                 new RevHubOrientationOnRobot(
@@ -40,18 +46,22 @@ public class CenterStageRobotHardware {
                 )
         );
 
-        Imu.initialize(newImuParameters);
+        Imu.initialize(newImuParameters);*/
 
+        //Reverse any motors that need it
         BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //Set all the motors to ZeroPowerBehavior so they all stop at the same time
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        //Sets the Claw to open in Initialization
         Claw.setPosition(0);
     }
 
+    //This function is called to stop all the motors on the robot in autonomous
     public void Stop(){
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
@@ -59,10 +69,13 @@ public class CenterStageRobotHardware {
         BackRight.setPower(0);
     }
 
+    //This function create a timer and takes inputs from when you call the function to tell the robot how far to move and what direction
     public void Drive(String dir, int blocks){
         MoveTime.reset();
-        int newTime = blocks * 1450;
+        //It takes 1450 milliseconds to move one block so we multiply that by the amount of blocks we want to go
+        double newTime = blocks * 1450;
         MoveTime.startTime();
+        //These check to see what we inputed to tell it what direction we want it to go
         if(dir == "Forward" && MoveTime.milliseconds() <= newTime){
             FrontLeft.setPower(1);
             FrontRight.setPower(1);
@@ -75,6 +88,7 @@ public class CenterStageRobotHardware {
             BackLeft.setPower(-1);
             BackRight.setPower(-1);
         }
+        //This calls the function created above to stop all motors when the robot has reached it's destination
         else{this.Stop();}
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageRobotHardwareTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageRobotHardwareTest.java
@@ -2,18 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+                //We set the type of file this is plus name it
 @Autonomous(name = "HardwareTesting")
 public class CenterStageRobotHardwareTest extends LinearOpMode {
 
+    //This creates a Instance of CenterStageRobotHardware and stores it to a variable. In this case we save it to "Robot"
     CenterStageRobotHardware Robot = new CenterStageRobotHardware();
     @Override
     public void runOpMode() {
+        //Since Robot is the Instance of CenterStageRobotHardware we have to initialize it to initialize all the motors
         Robot.Init(hardwareMap);
         waitForStart();
 
+        //This checks to see if the OpMode is running and if so runs the code inside of it
         if (opModeIsActive()){
+            //This will run anything in the curly brackets while the condition is true.
+            //In this case we already know that it is true when it enters. And then it remains true until we press stop
             while(opModeIsActive()){
+                //This is calling the Drive function in CenterStageRobotHardware. So we give it a direction and the distance to travel
                 Robot.Drive("Forward", 2);
             }
         }
